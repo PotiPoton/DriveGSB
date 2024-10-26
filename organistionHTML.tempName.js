bfc9df5e03c3a04ace82d67f2971e5bf02b434a4
PotiPoton <leo.gilblas@mailo.com>
2024-10-26 17:24:17 +0200

    v1

---
+++ organistionHTML.tempName.js
@@ -0,0 +1,37 @@
+function createEntireElement(element, attributes){
+    let elm = document.createElement(element);
+
+    for (let [key, value] of Object.entries(attributes)) {
+        if (value !== undefined) {
+            if (key === 'innerText') elm.innerText = value;
+            else if (key === 'innerHTML') elm.innerHTML = value;
+            else if (key === 'style' && typeof value === 'object') {
+                Object.assign(elm.style, value);
+            } else if (key.startsWith('on') && typeof value === 'function') {
+                elm.addEventListener(key.slice(2), value);
+            } else elm.setAttribute(key, value);
+        }
+    }
+    return elm;
+}
+
+//*Exemple d'utilisation de createEntireElement :
+// let div = createEntireElement('div', { id: 'cnt' });
+// let iptBtn = createEntireElement('input', { 
+//     type: 'button', 
+//     value: 'Bonjour toi', 
+//     onclick: function (e) {
+//         e.preventDefault();
+//         let existingP = document.getElementById('p');
+//         if (existingP) existingP.remove();
+//         else {
+//             let p = createEntireElement('p', { 
+//                 id: 'p', 
+//                 innerText: 'SALUT BOTÉDAIZIL' });
+//             document.body.appendChild(p);
+//         }
+//     }
+// });
+// document.body.appendChild(div);
+// document.body.appendChild(iptBtn);
+
---
+++ temp.api.php
@@ -0,0 +1,10 @@
+<?php
+
+if (isset($_GET['folder'])){
+    if($_GET['folder'] == 1){
+        require_once "temp.folder.json"; //TODO appeler la fonction qui génère le JSON
+    }
+}
+
+
+?>
